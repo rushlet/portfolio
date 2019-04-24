2f071d55a811fe5611e8f4076f9aebbe2ffa900f
Becky Rush <becky.rush@bbc.co.uk>
2019-04-24 11:49:47 -0700

    set up with new scaffold, move out a lot of files for now

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+console.log('I am webpack.');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+	entry: {
+	    app: [
+	        './src/app.js',
+	        './src/app.scss'
+	    ],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    /node_modules/
+                ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                },
+            },
+            {
+                test: /\.hbs$/,
+                use: [{
+                    loader: 'handlebars-loader',
+                    options: {
+                        helperDirs: [path.resolve(__dirname, 'src/components'), path.resolve(__dirname, 'src')],
+                    }
+                }]
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                 test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                 loader: 'url-loader?limit=100000'
+            }
+       ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.hbs'),
+            title: 'Webpack project set up', // needs updating with each project!
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ]
+};

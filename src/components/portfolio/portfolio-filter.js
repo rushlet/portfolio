de9e0a3b3db13556b77588a1db8767cebab0a88f
Becky Rush <becky.rush@bbc.co.uk>
2019-04-30 06:58:49 -0700

    refactor

--- src/components/portfolio/portfolio-filter.js
+++ src/components/portfolio/portfolio-filter.js
@@ -2,39 +2,18 @@ class PortfolioFilters {
     constructor() {
         this.filters = document.querySelectorAll('.filter-input');
         this.portfolioItems = document.querySelectorAll('.portfolio-item');
-        this.test = 'hello';
-        this.filters.forEach((filter) => {
-            console.log('filter', filter);
-            console.log('test', this.test);
-            filter.addEventListener('change', this.filterItems.bind(this));
-        });
+        this.filters.forEach((filter) => filter.addEventListener('change', this.filterItems.bind(this)));
     }
 
     getSelectedFilter() {
         return document.querySelector('input[name = "filter"]:checked').id;
     }
 
-    showItem(item) {
-        item.classList.add('show');
-        item.classList.remove('hide');
-    }
-
-    hideItem(item) {
-        item.classList.remove('show');
-        item.classList.add('hide');
-    }
-
     filterItems() {
         const selectedFilter = this.getSelectedFilter();
         this.portfolioItems.forEach((item) => {
-            if (selectedFilter === 'all' || item.classList.contains(selectedFilter)) {
-                this.showItem(item);
-            }
-            else {
-                this.hideItem(item);
-            }
+            (selectedFilter === 'all' || item.classList.contains(selectedFilter)) ? item.classList.remove('hide') : item.classList.add('hide');
         });
-
     }
 }
 

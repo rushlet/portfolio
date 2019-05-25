41ea4386e16dbf95250a4df5898ac59e5d428316
Becky Rush <becky.rush@bbc.co.uk>
2019-05-25 23:07:18 +0100

    style about section inc multiple about imgs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import PortfolioFilters from './components/portfolio/portfolio-filter';
+import About from './components/about/about';
 
-const portfolioFilters = new PortfolioFilters();
+new About();
+new PortfolioFilters();
 const message = "HELLO!"
 console.log(`This is written in es6 and working. ${message}`);
 
---
+++ src/components/about/about.js
@@ -0,0 +1,22 @@
+class About {
+    constructor() {
+        this.aboutShuffleBtn = document.querySelector('.about-content--img-shuffle');
+        this.aboutImgs = document.querySelectorAll('.img-me');
+        console.log('about imgs', this.aboutImgs);
+        this.aboutShuffleBtn.addEventListener('click', this.shuffleImg.bind(this));
+    }
+
+    shuffleImg() {
+        let selected = 0;
+        let next = 0;
+        this.aboutImgs.forEach((img, i) => {
+            if (img.style.display === 'block') selected = i;
+            img.style.display = 'none';
+        });
+        next = selected + 1;
+        if (selected === this.aboutImgs.length - 1) next = 0;
+        this.aboutImgs[next].style.display = 'block';
+    }
+}
+
+export default About;
